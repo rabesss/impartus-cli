d716c180a577ff343cc76cd62b73255c8ed09d9e
rabesss <26330469+rabesss@users.noreply.github.com>
2026-07-13 12:44:56 +0530

    test: drain stdout capture concurrently

--- internal/cli/cli_testhelpers_test.go
+++ internal/cli/cli_testhelpers_test.go
@@ -1,29 +1,14 @@
 package cli
 
 import (
-	"io"
 	"os"
 	"testing"
 )
 
 func captureStdout(t *testing.T, fn func() error) (string, error) {
 	t.Helper()
-	oldStdout := os.Stdout
-	r, w, err := os.Pipe()
-	if err != nil {
-		t.Fatalf("os.Pipe: %v", err)
-	}
-	os.Stdout = w
-	runErr := fn()
-	//nolint:errcheck // closing write end of pipe in test helper
-	w.Close()
-	os.Stdout = oldStdout
-
-	out, readErr := io.ReadAll(r)
-	if readErr != nil {
-		t.Fatalf("ReadAll: %v", readErr)
-	}
-	return string(out), runErr
+	stdout, _, err := captureOutputStreams(t, fn)
+	return stdout, err
 }
 
 func restoreCLIState(t *testing.T) {
